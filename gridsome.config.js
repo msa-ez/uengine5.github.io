b43562066ae0d6e0a78df27c928575211f4ee6e9
kibum0405 <kibum0405@users.noreply.github.com>
2025-03-07 08:08:37 +0000

    deploy: 15dc6e5f312a952815ebd692d6710d39de6f8d12

---
+++ gridsome.config.js
@@ -0,0 +1,186 @@
+// This is where project configuration and plugin options are located. 
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: '',
+  // 주석 부분 : uengine6 뒤에 각 리스트의 타이틀을 출력하고 싶을때 사용
+  // titleTemplate: 'uengine6 - %s',
+  titleTemplate: 'uengine6',
+  icon: {
+    favicon: './src/assets/favicon.png',
+    touchicon: './src/assets/favicon.png'
+  },
+  siteUrl: (process.env.SITE_URL ? process.env.SITE_URL : 'https://intro.bpm-getting-started5.io'),
+  settings: {
+    web: process.env.URL_WEB || false,
+    twitter: process.env.URL_TWITTER || false,
+    github: process.env.URL_GITHUB || false,
+    nav: {
+      links: [
+        { path: '/getting-started/', title: 'Docs' }
+      ]
+    },
+    sidebar: [
+      {
+        name: 'getting-started',
+        sections: [
+          {
+            title: 'uEngine6 BPM',
+            items: [
+              '/bpm6-intro/',
+              '/bpm6-intro/business-process/',
+              '/bpm6-intro/example-video/',
+              // '/getting-started/bpm6-introduce/',
+              // '/getting-started/bpm6-modeling/',
+              // '/getting-started/modeling-tools/',
+              // '/getting-started/database-table/',
+              // '/getting-started/form-definition/',
+              // '/getting-started/form-mapping/',
+              // '/getting-started/conditional-branch/',
+              // '/getting-started/multiple-instance/',
+              // '/getting-started/process-monitoring/',
+              // '/getting-started/soa-architecture-event/',
+              // '/getting-started/soa-architecture-message/',
+              // '/getting-started/bpm6-admin/',
+              // '/getting-started/bpm6-data-management/',
+            ]
+          },
+          {
+            title: '배포 및 설정 가이드',
+            items: [
+              '/bpm6-deploy-config/',
+              '/bpm6-deploy-config/bpm6-account/',
+              '/bpm6-deploy-config/bpm6-deploy/',
+              '/bpm6-deploy-config/uengine3-migration/',
+            ]
+          },          
+          {
+            title: '모델링 및 실행',
+            items: [
+              '/bpm6-started/',
+              '/bpm6-started/bpm6-modeling/',
+              '/bpm6-started/form-definition/',
+              '/bpm6-started/form-mapping/',
+              '/bpm6-started/conditional-branch/',
+              '/bpm6-started/multiple-instance/',
+              // '/bpm6-started/script-task/',
+              '/bpm6-started/bpm6-external/',
+              '/bpm6-started/bpm6-execution/',
+              
+              // '/api-customizing/api-reference/',
+              // '/api-customizing/message-listener/',
+              // '/api-customizing/create-project/',
+              // '/api-customizing/new-activity/',
+              // '/api-customizing/migration/',
+              // '/api-customizing/transaction-listener/',
+              // '/api-customizing/script-task/',
+              // '/api-customizing/bpm6-system-integration/',
+            ]
+          },
+          {
+            title: '운영 및 데이터관리',
+            items: [
+              '/bpm6-admin/',
+              // '/bpm6-admin/bpm6-admin-instance/',
+              '/bpm6-admin/bpm6-file/',
+              '/bpm6-admin/bpm6-database/',
+            ]
+          },
+          {
+            title: '프로세스 API',
+            items: [
+              '/bpm6-api/',
+              '/bpm6-api/bpm6-message-listener/',
+              '/bpm6-api/bpm6-transaction-listener/',
+            ]
+          },
+          // {
+          //   title: 'AI를 활용한 프로세스 모델링 가이드',
+          //   items: [
+          //     '/ai-generating/',
+          //     '/ai-generating/process-definition/',
+          //     '/ai-generating/form-definition/',
+          //   ]
+          // },
+          {
+            title: 'BPMN 프로젝트 예제',
+            items: [
+              '/process-example/',
+              // '/process-example/sub-process/',
+              // '/process-example/call-activity/',
+            ]
+          },
+          {
+            title: '프로세스 자산 라이브러리(PAL)',
+            items: [
+              '/pal-edition/',
+              '/pal-edition/process-definition-map/',
+              '/pal-edition/process-definition-edit/',
+              // '/pal-edition/phase/',
+              // '/pal-edition/save-image/',
+            ]
+          },
+          {
+            title: '프로세스 질의 응답 (Process-GPT)',
+            items: [
+              '/process-gpt/ai-chat-guide/',
+            ]
+          },
+          {
+            title: '문의하기',
+            items: [
+              '/bpm6-contact/',
+            ]
+          },
+        ]
+      }
+    ]
+  },
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        baseDir: './content',
+        path: '**/*.md',
+        typeName: 'MarkdownPage',
+        remark: {
+          externalLinksTarget: '_blank',
+          externalLinksRel: ['noopener', 'noreferrer'],
+          plugins: [
+            '@gridsome/remark-prismjs'
+          ]
+        }
+      }
+    },
+
+    {
+      use: 'gridsome-plugin-tailwindcss',
+      options: {
+        tailwindConfig: './tailwind.config.js',
+        purgeConfig: {
+          // Prevent purging of prism classes.
+          whitelistPatternsChildren: [
+            /token$/
+          ]
+        }
+      }
+    },
+
+    {
+      use: '@gridsome/plugin-google-analytics',
+      options: {
+        id: (process.env.GA_ID ? process.env.GA_ID : 'XX-999999999-9')
+      }
+    },
+
+    {
+      use: '@gridsome/plugin-sitemap',
+      options: {  
+      }
+    }
+
+  ]
+}
---
+++ src/templates/MarkdownPage.vue
@@ -0,0 +1,194 @@
+<template>
+  <Layout>
+      <div class="flex flex-wrap items-start justify-start">
+
+        <div class="order-2 w-full md:w-1/4 sm:pl-4 md:pl-6 lg:pl-8 sticky" style="top: 4rem">
+          <OnThisPage />
+        </div>
+
+        <div class="order-1 w-full md:w-3/4">
+          <div class="content" v-html="$page.markdownPage.content" />
+
+          <div class="mt-8 pt-8 lg:mt-12 lg:pt-12 border-t border-ui-border">
+            <NextPrevLinks />
+          </div>
+        </div>
+
+      </div>
+  </Layout>
+</template>
+
+<page-query>
+query ($id: ID!) {
+  markdownPage(id: $id) {
+    id
+    title
+    description
+    path
+    timeToRead
+    content
+    sidebar
+    next
+    prev
+    headings {
+      depth
+      value
+      anchor
+    }
+  }
+  allMarkdownPage{
+    edges {
+      node {
+        path
+        title
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+import OnThisPage from '@/components/OnThisPage.vue';
+import NextPrevLinks from '@/components/NextPrevLinks.vue';
+import Clipboard from 'clipboard'
+
+export default {
+  components: {
+    OnThisPage,
+    NextPrevLinks
+  },
+  mounted() {
+    this.$nextTick(function () {
+      this.instructionCodeBlockClipBoard()
+    })
+  },
+  updated() {
+    this.$nextTick(function () {
+      // 전체 화면내용이 다시 렌더링된 후에 아래의 코드가 실행됩니다. 
+      this.instructionCodeBlockClipBoard()
+    })
+  },
+
+  methods: {
+    instructionCodeBlockClipBoard() {
+      // al pre tags on the page
+      const pres = document.getElementsByTagName("pre")
+      // reformat html of pre tags
+      if (pres !== null) {
+          for (let i = 0; i < pres.length; i++) {
+              // check if its a pre tag with a prism class
+              if (isPrismClass(pres[i])) {
+                  // insert code and copy element
+                  // 이미지 저장 및 호출 https://github.com/msa-ez/uengine5.github.io/blob/main/README.md
+                  pres[i].innerHTML = `<div class="markdown-body copy"><img style="" src="https://user-images.githubusercontent.com/59447401/137833005-f4f6b7b0-ac2e-4dc6-a414-3bc132b4919a.png" /></div><code class="markdown-body ${pres[i].className}">${pres[i].innerHTML}</code>`
+              }
+          }
+      }
+      // create clipboard for every copy element
+      const clipboard = new Clipboard('.copy', {
+          target: (trigger) => {
+              return trigger.nextElementSibling;
+          }
+      });
+      // do stuff when copy is clicked
+      clipboard.on('success', (event) => {
+          // event.trigger.textContent = '📄';
+          setTimeout(() => {
+              event.clearSelection();
+              // event.trigger.textContent = '📝';
+          }, 500);
+      });
+      // helper function
+      function isPrismClass(preTag) {
+          return preTag.className.substring(0, 9).indexOf('language') != -1
+      }
+    }
+  },
+  
+  metaInfo() {
+    const title = this.$page.markdownPage.title;
+    const description = this.$page.markdownPage.description || this.$page.markdownPage.excerpt;
+
+    return {
+      title: title,
+      meta: [
+        {
+          name: 'description',
+          content: description
+        },
+        {
+          key: 'og:title',
+          name: 'og:title',
+          content: title,
+        },
+        {
+          key: 'twitter:title',
+          name: 'twitter:title',
+          content: title,
+        },
+        {
+          key: 'og:description',
+          name: 'og:description',
+          content: description,
+        },
+        {
+          key: 'twitter:description',
+          name: 'twitter:description',
+          content: description,
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+@import 'prism-themes/themes/prism-material-oceanic.css';
+
+/* 클립보드 스타일 */
+:not(pre) > code[class*="language-"] {
+  white-space: pre;
+  overflow-x: auto;
+}
+pre[class*=language-] {
+  position: relative;
+  overflow: auto;
+}
+
+code[class*=language-]:nth-child(1) {
+  margin-left:-1px;
+}
+
+pre[class*=language-] .copy {
+  content: "copy";
+  position: absolute;
+  width:32px;
+  height:32px;
+  filter: invert(100%);
+  top: 0;
+  right: 0;
+  margin-top:35px;
+  margin-right:-8px;
+  transform: translate(-10px, -90%);
+  cursor: pointer;
+  transition: all 200ms ease;
+}
+
+pre[class*=language-]:hover .copy {
+  transform: translate(-10px, -100%);
+}
+pre[class*=language-]:hover .copy:hover {
+  color: gray;
+}
+code {
+  overflow: scroll;
+  border-style:none;
+}
+code ::selection {
+		color: #5967d8;
+}
+code[class*="language-"]::selection, 
+pre[class*="language-"]::selection, 
+code[class*="language-"] ::selection,
+pre[class*="language-"] ::selection{background-color:white;}
+</style>
